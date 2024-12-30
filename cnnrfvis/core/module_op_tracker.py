fix: fix receptive field computation when stride is greater than 1

cnnrfvis/core/module_op_tracker.py
@@ -10,6 +10,7 @@ class CatchEachOp(TorchDispatchMode):
         super().__init__()
         self.verbose = verbose
         self.template = None
+        self.prev_stride = None
         self.conv_layer_idx = 0
 
     def __torch_dispatch__(self, func, types, args=..., kwargs=None):
@@ -21,20 +22,27 @@ class CatchEachOp(TorchDispatchMode):
             if self.conv_layer_idx == 0:
                 self.template = torch.ones(k_h, k_w)
             else:
-                self.template += 1
-                self.template = F.pad(
-                    self.template,
-                    (int(k_w / 2), int(k_w / 2), int(k_h / 2), int(k_h / 2)),
-                    value=1)
+                width = (k_w - 1) * self.prev_stride[1] + self.template.shape[1]
+                height = (k_h - 1) * self.prev_stride[0] + self.template.shape[0]
+                template = torch.zeros(height, width)
+                for i in range(k_h):
+                    for j in range(k_w):
+                        start_h = i * self.prev_stride[0]
+                        end_h = start_h + self.template.shape[0]
+                        start_w = j * self.prev_stride[1]
+                        end_w = start_w + self.template.shape[1]
+                        template[start_h:end_h, start_w:end_w] += self.template
+                self.template = template
 
             if self.verbose:
                 print(
                     f"Kernel_Size: {weight.shape[-2:]}, Stride: {stride}, Padding: {padding}"
                 )
             print(
-                f"ReceptiveFileds for {self.conv_layer_idx}-th conv: {self.template}"
+                f"ReceptiveFileds for {self.conv_layer_idx}-th conv: {self.template}, {self.template.shape}"
             )
 
             self.conv_layer_idx += 1
+            self.prev_stride = stride
 
         return func(*args, **(kwargs or {}))

tests/SimpleCNN.py
@@ -51,6 +51,6 @@ print(model)
 # 随机生成输入张量
 input_tensor = torch.randn(1, 3, 32, 32)
 
-with CatchEachOp(verbose=False):
+with CatchEachOp(verbose=True):
     # 前向传播
     output = model(input_tensor)